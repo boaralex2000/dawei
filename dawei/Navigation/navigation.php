4681722c9ce2ac9ab743a1d1dce08a36338c6508
boaralex2000 <boaralex10@gmail.com>
2018-02-09 20:07:42 +0100

    Creation part 1

---
+++ dawei/Navigation/navigation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="../css/css.css" />
+<link rel="stylesheet" href="../css/dish.css" />
+
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="/dawei/home/">Home</a></li>
+  <li><a href="/dawei/menu/">Menu</a></li>
+  <li><a href="/dawei/about/">About</a></li>
+</ul>
